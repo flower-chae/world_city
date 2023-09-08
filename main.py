30b2df48770921bd0f98f479aba42308984540f8
cskrub33 <cskrub33@gmail.com>
2023-09-09 08:24:10 +0900

    added word boundary logic

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import re
 
 # CSV 파일 읽기
-df = pd.read_csv('csk_city.csv')
+df = pd.read_csv('csk_nation_city.csv')
 
 # 정규 표현식 패턴 생성
 pattern_dict = {} # 정규 표현식 패턴을 저장할 딕셔너리
@@ -10,6 +10,11 @@ pattern_dict = {} # 정규 표현식 패턴을 저장할 딕셔너리
 for index, row in df.iterrows():
     expected = row['Expected']
     pattern_str = row['ExpectedReRef']
+
+    # 패턴에 \b를 추가하여 단어 경계를 지정
+    pattern_str = r'\b' + pattern_str + r'\b'
+
+    
     pattern = re.compile(pattern_str, re.IGNORECASE)
     pattern_dict[expected] = pattern
 
@@ -23,11 +28,18 @@ while True:
     # 사용자 입력이 'quit'인지 확인하고 종료
     if user_input.lower() == 'quit':
         break
+
+    matched = False
+
     # 입력된 도시 이름을 대응하는 패턴으로 변환
     for expected, pattern in pattern_dict.items():
         if pattern.search(user_input):
             user_input = expected
+            matched = True
             break
+
+    if not matched:
+        print("일치하는 도시를 찾을 수 없습니다.")
 
     # 변환된 결과 출력
     print(user_input)
